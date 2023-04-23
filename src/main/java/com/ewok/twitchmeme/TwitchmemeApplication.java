d0669242d37d19baf49ad2834e11c73d64dbbbce
KHS <ewoking777@gmail.com>
2023-04-23 16:39:08 +0900

    프로젝트 생성

---
+++ src/main/java/com/ewok/twitchmeme/TwitchmemeApplication.java
@@ -0,0 +1,13 @@
+package com.ewok.twitchmeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchmemeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitchmemeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ewok/twitchmeme/TwitchmemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewok.twitchmeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitchmemeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
