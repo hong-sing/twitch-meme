f10a1906aae64f12169d516d0b0924877dfb176f
KHS <ewoking777@gmail.com>
2023-04-29 20:09:28 +0900

    배포 자동화 구성

--- src/main/java/com/ewok/twitchmeme/TwitchmemeApplication.java
+++ src/main/java/com/ewok/twitchmeme/TwitchmemeApplication.java
@@ -2,6 +2,7 @@ package com.ewok.twitchmeme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -9,7 +10,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class TwitchmemeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TwitchmemeApplication.class, args);
+        SpringApplication application = new SpringApplication(TwitchmemeApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 
 }
