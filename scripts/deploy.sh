e3b77b51424a46082b064e5a81c9e890040bc8b9
KHS <ewoking777@gmail.com>
2023-04-29 21:33:57 +0900

    무중단 배포 구성 (1) - /profile 호출 시 oauth가 나와 다시 설정 4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep java | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl twitch-meme | grep java | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
@@ -34,6 +34,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
         -Dspring.profile.active=real \
 $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
